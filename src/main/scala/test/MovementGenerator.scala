3852c726cbdbcc3b6d2ca3a57ae304a868562d60
saz <shafiq.akram.zakaria@gmail.com>
2016-10-09 11:26:31 +0800

    Check-in MovementGenerator and its unit test.

---
+++ src/main/scala/test/MovementGenerator.scala
@@ -0,0 +1,81 @@
+package test
+
+/**
+  * Created by saz on 10/8/2016.
+  */
+object MovementGenerator {
+
+
+  private
+  def withinChessBoard(boardWidth: Int, boardHeight: Int): (Position) => Boolean = {
+    p => {
+      p.x <= boardWidth &&
+      p.x > 0 &&
+      p.y > 0 &&
+      p.y <= boardHeight
+    }
+  }
+
+  def upperLeft(pos: Position, width: Int, height: Int): List[Position] = {
+
+    val xs = List.range(pos.x - 1, 0, -1)
+    var y = pos.y
+
+    xs.map( x => { y = y + 1; Position(x, y) })
+      .filter(withinChessBoard(width, height))
+  }
+
+  def lowerLeft(pos: Position, width: Int, height: Int): List[Position] = {
+
+    val xs = List.range(pos.x - 1, 0, -1)
+    var y  = pos.y
+
+    xs.map( x => { y = y - 1; Position(x, y) })
+      .filter(withinChessBoard(width, height))
+  }
+
+  def lowerRight(pos: Position, width: Int, height: Int): List[Position] = {
+
+    val xs = List.range(pos.x + 1, height + 1)
+    var y  = pos.y
+
+    xs.map(x => { y = y - 1; Position(x, y) })
+      .filter(withinChessBoard(width, height))
+  }
+
+  def upperRight(pos: Position, width: Int, height: Int): List[Position] = {
+
+    val xs = List.range(pos.x + 1, height + 1)
+    var y  = pos.y
+
+    xs.map( x => { y = y + 1; Position(x, y) })
+      .filter(withinChessBoard(width, height))
+  }
+
+  def topToBottom(position: Position, boardWidth: Int, boardHeight: Int) : List[Position] = {
+
+    for (y <- List.range(1, boardHeight + 1)
+         if y != position.y)
+    yield Position(position.x, y)
+  }
+
+  def leftToRight(position: Position, boardWidth: Int, boardHeight: Int) : List[Position] = {
+
+    for (x <- List.range(1, boardWidth + 1)
+         if x != position.x)
+    yield Position(x, position.y)
+  }
+
+  def knightMoves(position: Position, boardWidth: Int, boardHeight: Int) : List[Position] = {
+
+    (Position(position.x + 2, position.y + 1) ::
+     Position(position.x + 2, position.y - 1) ::
+     Position(position.x - 2, position.y + 1) ::
+     Position(position.x - 2, position.y - 1) ::
+     Position(position.x + 1, position.y + 2) ::
+     Position(position.x + 1, position.y - 2) ::
+     Position(position.x - 1, position.y + 2) ::
+     Position(position.x - 1, position.y - 2) ::
+     List()).filter(withinChessBoard(boardWidth, boardHeight))
+  }
+}
---
+++ src/test/scala/test/MovementGeneratorSpec.scala
@@ -0,0 +1,83 @@
+package test
+
+import org.scalatest._
+/**
+  * Created by saz on 10/8/2016.
+  */
+class MovementGeneratorSpec extends FlatSpec with Matchers {
+
+  val mg = test.MovementGenerator
+
+  "upperLeft" should "move north west" in {
+
+    val p = Position(3, 1)
+
+    assert(mg.upperLeft(p, 3, 3).equals(Position(2, 2)::Position(1, 3)::List()))
+  }
+
+  "bottomLeft" should "move south west" in {
+
+    val p = Position(3, 2)
+
+    assert(mg.lowerLeft(p, 3, 3).equals(Position(2, 1)::List()))
+  }
+
+  "leftToRight" should "move horizontally" in {
+
+    val p = Position(2,3)
+
+    val expected = Position(1,3) ::
+                   Position(3,3) ::
+                   Position(4,3) ::
+                   Position(5,3) ::
+                   List()
+
+    assert(mg.leftToRight(p, 5, 5).equals(expected))
+  }
+
+  "topToBottom" should "move vertically" in {
+
+    val p = Position(2,2)
+
+    val expected = Position(2,1) ::
+                   Position(2,3) ::
+                   Position(2,4) ::
+                   Position(2,5) ::
+                   List()
+
+    assert(mg.topToBottom(p, 5, 5).equals(expected))
+  }
+
+  "lowerRight" should "move south east" in {
+
+    val p = Position(1, 3)
+
+    assert(mg.lowerRight(p, 3, 3).equals(Position(2, 2)::Position(3, 1)::List()))
+  }
+
+  "upperRight" should "move north east" in {
+
+    val p = Position(1, 2)
+
+    assert(mg.upperRight(p, 4, 4).equals(Position(2, 3)::Position(3, 4)::List()))
+  }
+
+  "knightMoves" should "Jump like a knight" in {
+
+    val p = Position(3, 3)
+
+    val expected = Position(5,4) ::
+                   Position(5,2) ::
+                   Position(1,4) ::
+                   Position(1,2) ::
+                   Position(4,5) ::
+                   Position(4,1) ::
+                   Position(2,5) ::
+                   Position(2,1) ::
+                   List()
+    assert(mg.knightMoves(p, 8, 8).equals(expected))
+
+    assert(mg.knightMoves(Position(1, 3), 3, 3)
+             .equals(Position(3, 2)::Position(2, 1)::List()))
+  }
+}
