aa0dd2e1e512996daa3952d9a4aaa0ae1505c470
saz <shafiq.akram.zakaria@gmail.com>
2016-10-10 14:41:17 +0800

    Add method movablePositions to the trait Piece

--- src/main/scala/test/Piece.scala
+++ src/main/scala/test/Piece.scala
@@ -5,19 +5,43 @@ import scala.collection.mutable.{MutableList => MList};
   * Created by saz on 10/8/2016.
   */
 sealed trait Piece {
+
+  val mg = MovementGenerator
+
+  def
+  movablePositions(position: Position, boardWith: Int, boardHeight: Int) :
+  List[Position]
 }
 
 final class King extends Piece {
+
+  override
+  def movablePositions(position: Position, boardWith: Int, boardHeight: Int):
+  List[Position] = mg.kingMoves(position, boardWith, boardHeight);
 }
 
 final class Queen extends Piece {
+
+  override
+  def movablePositions(position: Position, boardWith: Int, boardHeight: Int):
+  List[Position] = mg.queenMoves(position, boardWith, boardHeight)
 }
 
 final class Bishop extends Piece {
+  override
+  def movablePositions(position: Position, boardWith: Int, boardHeight: Int):
+  List[Position] = mg.bishopMoves(position, boardWith, boardHeight)
 }
 
 final class Rook extends Piece {
+  override
+  def movablePositions(position: Position, boardWith: Int, boardHeight: Int):
+  List[Position] = mg.rookMoves(position, boardWith, boardHeight)
 }
 
 final class Knight extends Piece {
+
+  override
+  def movablePositions(position: Position, boardWith: Int, boardHeight: Int):
+  List[Position] = mg.knightMoves(position, boardWith, boardHeight)
 }
