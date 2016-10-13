10aa6ca3ffd357bac6d7ce09766f279a31b59718
saz <shafiq.akram.zakaria@gmail.com>
2016-10-13 16:44:44 +0800

    Add comments to the class Position.

--- src/main/scala/test/Position.scala
+++ src/main/scala/test/Position.scala
@@ -2,6 +2,9 @@ package test
 
 /**
   * Created by saz on 10/8/2016.
+  * Describes a position on the chess board.
+  * Note that (1, 1) means 1st row, 1st column where the square is located in
+  * the lower left, just like a normal x-y axis graph.
   */
 case class Position(x:Int, y:Int) {
 }
