b5847317cfbc762edd648d76310bf02185ccb62d
saz <shafiq.akram.zakaria@gmail.com>
2016-10-13 16:46:46 +0800

    Add comment to Placement.

--- src/main/scala/test/Placement.scala
+++ src/main/scala/test/Placement.scala
@@ -2,6 +2,9 @@ package test
 
 /**
   * Created by saz on 10/9/2016.
+  * Represents a {@code Piece} on when it's put on the chess board.
+  * @see test.Piece
+  * @see test.Position
   */
 case class Placement(piece : Piece, position: Position) {
 
